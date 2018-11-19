24b49c002d9be519422ffad22fb39ccd26dab6f0
Asher Rosenberg <38874671+asherrose@users.noreply.github.com>
2018-11-19 17:48:23 -0500

    Update WeightLineChart.java
    Changed x-axis to dates

--- WeightTracker/src/WeightLineChart.java
+++ WeightTracker/src/WeightLineChart.java
@@ -10,17 +10,17 @@ public class WeightLineChart extends Application {
 	@Override
 	public void start(Stage stage) {
 		stage.setTitle("Weight History");
-		final NumberAxis xAxis = new NumberAxis();
+		final CategoryAxis xAxis = new CategoryAxis();
 		final NumberAxis yAxis = new NumberAxis();
 		xAxis.setLabel("Count of Weigh In");
 		yAxis.setLabel("Weight");
 		
-		final LineChart<Number, Number> lineChart = new LineChart<Number, Number>(xAxis, yAxis);
+		final LineChart<String, Number> lineChart = new LineChart<String, Number>(xAxis, yAxis);
 		lineChart.setTitle("User's Weight History");
 		XYChart.Series series = new XYChart.Series();
 		series.setName(WeightServlet.n + "'s Weight");
-		for (int i = 1; i <= Welcome.intArray.length; i++) {
-			series.getData().add(new XYChart.Data(i, Welcome.intArray[i - 1]));
+		for (int i = 0; i < Welcome.intArray.length; i++) {
+			series.getData().add(new XYChart.Data(Welcome.dateSplit[i], Welcome.intArray[i]));
 		}
 		Scene scene = new Scene(lineChart, 800, 600);
 		lineChart.getData().add(series);
